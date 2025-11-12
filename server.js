d11c1336a412e01547511afe77a1e0da4cab9c51
usuario <ti2@ativita.net>
2025-11-12 14:00:00 -0300

    resuelto problema apikey

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-// TODO: Mover esto a variables de entorno antes de ir a producción - no olvidar
-const API_KEY = "dj-walc-2023-super-secreto-y-muy-largo-para-ser-encontrado";
+// la api key esta en las variables de entorno
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
