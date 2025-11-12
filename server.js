996b0fd7912f01e2bc5c2bf988eb00f22b454bf9
usuario <ti2@ativita.net>
2025-11-12 13:54:25 -0300

    mejora en integracion con api externa

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-// TODO: Mover esto a variables de entorno antes de ir a producción
+// TODO: Mover esto a variables de entorno antes de ir a producción - no olvidar
 const API_KEY = "dj-walc-2023-super-secreto-y-muy-largo-para-ser-encontrado";
 
 app.use(express.static(path.join(__dirname, 'public')));
