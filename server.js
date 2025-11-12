d0ea0b0e1862aa692410ab5ecfeaedaa78440228
usuario <ti2@ativita.net>
2025-11-12 13:39:38 -0300

    apikey

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+// TODO: Mover esto a variables de entorno antes de ir a producción
+const API_KEY = "dj-walc-2023-super-secreto-y-muy-largo-para-ser-encontrado";
 
 app.use(express.static(path.join(__dirname, 'public')));
 
